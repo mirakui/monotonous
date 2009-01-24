358b02b6573bfd9ddcdc33e676367a88ed6fd04e
mirakui <mimitako@gmail.com>
2009-01-25 01:19:13 +0900

    Added direct messages

--- doppelkun/doppelkun.rb
+++ doppelkun/doppelkun.rb
@@ -8,10 +8,13 @@ require 'twitter'
 require 'pp'
 require 'optparse'
 require 'uri'
-require File.join BASE_DIR, 'lib/file_db'
+require 'open-uri'
+require 'hpricot'
+require File.join(BASE_DIR, 'lib/file_db')
 
-$target_file   = Gena::FileDB.new 'tmp/target',        :base=>__FILE__
-$since_id_file = Gena::FileDB.new 'tmp/since_id',      :base=>__FILE__
+$target_file         = Gena::FileDB.new 'tmp/target',         :base=>__FILE__
+$since_id_file       = Gena::FileDB.new 'tmp/since_id',       :base=>__FILE__
+$reply_since_id_file = Gena::FileDB.new 'tmp/reply_since_id', :base=>__FILE__
 
 $log = Logger.new(File.join(BASE_DIR, 'log', 'doppelkun.log'), 'daily')
 $log.level = $DEBUG ? Logger::DEBUG : Logger::INFO
@@ -26,7 +29,7 @@ def retarget(tw, target=nil)
     friends = tw.my(:followers).map {|f| f.screen_name}
     $log.debug "followers=(#{friends.length})#{friends.join ','}"
 
-    target = friends[rand friends.length]
+    target = friends[rand(friends.length)]
   end
   $log.debug "target=#{target}"
 
@@ -40,6 +43,8 @@ def retarget(tw, target=nil)
 
   #tw.status :post, ". @#{target_old} -> @#{target}"
   tw.status :post, ". @#{target_old} -> ?"
+  tw.message :post, 'さようなら', target_old
+  tw.message :post, '今日はあなたに決めた', target
   $log.info "retarget @#{target_old} -> @#{target}"
 
   $log.info 'end retarget'
@@ -75,6 +80,65 @@ def mirror_post(tw)
     $log.info "no statuses to mirror"
   end
   $log.info 'end mirror_post'
+
+  forward_replies(tw)
+end
+
+def forward_replies(tw)
+  $log.info('begin forward_replies')
+  reply_uri = 'http://twitter.com/statuses/replies.rss'
+  target = $target_file.read || retarget(tw)
+  auth = [tw.send(:login), tw.send(:password)]
+  reply_since_id = $reply_since_id_file.read
+  rss = ''
+  open(reply_uri, :http_basic_authentication=>auth) do |f|
+    rss = f.read
+  end
+  first = true
+  h = Hpricot(rss)
+  (h/'item').each do |item|
+    status_uri   = (item/'guid').text
+    status_id    = status_uri.split('/').last
+
+    if first
+      if reply_since_id != status_id
+        $reply_since_id_file.write status_id
+        $log.info "wrote reply_since_id #{status_id}"
+      else
+        $log.info "didn't wrote reply_since_id #{status_id}"
+      end
+      first = false
+    end
+
+    if reply_since_id == status_id
+      $log.debug "breaked by status_id #{status_id}"
+      break
+    end
+
+    description  = (item/'description').text
+    from, status = *(description.scan(/^([^:\s]+):\s(.*)$/).first)
+    status       = URI.escape(status, /[&]/) 
+    if from == target 
+      $log.info "didn't sent a message because from == target(#{target}), status_id:#{status_id}"
+    elsif from == tw.send('login')
+      $log.info "didn't sent a message because from == #{target}, status_id:#{status_id}"
+    else
+      message = "@#{from} が「#{status}」だって"
+      tw.message :post, message, target
+      $log.info "sent a message '#{message}' to #{target}"
+    end
+  end
+
+  $log.info('end forward_replies')
+end
+
+def announce_target(tw)
+  $log.info('begin announce_target')
+  target = $target_file.read || retarget(tw)
+  status = "今日は @#{target} に憑いてる"
+  tw.status :post, status
+  $log.info "sent a status '#{status}'"
+  $log.info('end announce_target')
 end
 
 # main
@@ -89,7 +153,14 @@ begin
 
   tw = Twitter::Client.new pit
 
-  task=='retarget' ? retarget(tw, ARGV.shift) : mirror_post(tw)
+  case task
+  when 'retarget'
+    retarget(tw, ARGV.shift)
+  when 'announce'
+    announce_target(tw)
+  else
+    mirror_post(tw)
+  end
 rescue => e
   $log.error "#{e.class}:#{e.to_str} -- #{e.backtrace}"
 end
