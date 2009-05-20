2202cfa4018d3fcb7c0498b5c4e3b537dbf10085
成田 一生 <mimitako@gmail.com>
2009-05-20 23:20:44 +0900

    返却POSTを廃止して発送のみに。発送POSTをDVDタイトルごとに分割

--- mirakui_dmm/plugin/dmm_plugin.rb
+++ mirakui_dmm/plugin/dmm_plugin.rb
@@ -13,10 +13,7 @@ module MirakuiDmm
       mails.each do |mail|
         from = mail[:header][/^Subject:.*$/]
         logger.debug "FROM: #{from.inspect}"
-        if from[/ご返却確認いたしました/]
-          logger.debug '返却!!'
-          process_return_mail mail
-        elsif from[/商品発送のお知らせ/]
+        if from[/商品発送のお知らせ/]
           logger.debug '発送!!'
           process_dispatch_mail mail
         end
@@ -29,26 +26,17 @@ module MirakuiDmm
       mails = imap.fetch( ['FROM', 'info@dmm.com', 'UNSEEN'] )
     end
 
-    def process_return_mail(mail)
-      titles = []
-      mail[:body].scan(/^\d{4}-\d{2}-\d{2}\s+\w+\s+(.*)$/) do |line|
-        title = $1
-        logger.debug "extracted from return_mail: #{title.inspect}"
-
-        titles << title
-      end
-      post "#{titles.map{|t| "「#{t}」"}}を返却しました"
-    end
-
     def process_dispatch_mail(mail)
       titles = []
       mail[:body].scan(/^\d{4}-\d{2}-\d{2}\s+\w+\s+(.*)$/) do |line|
         title = $1
         logger.debug "extracted from dispatch: #{title.inspect}"
 
-        titles << title
+        titles << title.chomp
       end
-      post "#{titles.map{|t| "「#{t}」"}}を発送しました"
+      titles.each do |title|
+        post "「#{title}」を発送しました"
+      end
     end
 
   end
